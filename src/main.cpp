984c16f367740cd200f20135e1bf70af0fcf9660
Baocong <18738129813@163.com>
2024-08-21 16:58:57 +0800

    first commit :进行对ws2812灯的测试 以及基本功能验证

---
+++ src/main.cpp
@@ -0,0 +1,59 @@
+#include <Arduino.h>
+#include <FastLED.h>
+
+#define NUM_LEDS 64 //ws2812 led总数量
+#define DATA_PIN 4  //数据引脚
+#define numColors 7 //七彩颜色变量
+
+
+CRGB leds[NUM_LEDS];
+
+CRGB colors[] = {CRGB::Red, CRGB::Orange, CRGB::Yellow, CRGB::Green, CRGB::Blue, CRGB::Indigo, CRGB::Violet};//定义七彩颜色数组 indigo:紫罗兰色 violet:紫色
+
+
+
+void setup() {
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+}
+
+void loop() {
+  for(int i = 0; i < 7; i++) {
+  int colorIndex = i % numColors;
+  for(int dot = 0; dot < NUM_LEDS; dot++) {
+    leds[dot] = colors[colorIndex];
+    FastLED.show();
+    delay(50);
+    }
+  for(int dot = 0; dot < NUM_LEDS; dot++) {
+    leds[dot] = CRGB::Black;
+    FastLED.show();
+    delay(50);
+  }
+  }
+}
+/*
+  for(int dot = 0 ;dot < NUM_LEDS;dot++){
+  leds[dot] = CRGB::Orange;
+  FastLED.show();
+  delay(30);
+
+  leds[dot] = CRGB::Black;
+  FastLED.show();
+  delay(30);
+  }
+*/
+
+/* :进行电位器控制灯数量实例
+void loop(){
+  int val = analogRead(2);
+  int numledsToLight = map(val, 0, 1023, 0, NUM_LEDS);
+
+  FastLED.clear();
+
+  for(int i = 0; i < numledsToLight; i++){
+    leds[i] = CRGB::yellow;
+  }
+
+  FastLED.show();
+}
+*/
